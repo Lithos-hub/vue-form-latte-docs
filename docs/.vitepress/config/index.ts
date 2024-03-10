d7c428ad2e6bf0fcc6e1204996e0c3bd5b817f62
Carlos Segura Garcia <lithos.contact@gmail.com>
2024-03-10 09:08:01 +0100

    refactor config file

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,49 +1,54 @@
-import { defineConfig } from "vitepress";
-import { en } from "../../locales/en";
-import { es } from "../../locales/es";
+import { DefaultTheme, defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: 'Vue Form Latte',
-  description: 'Form generator library for Vue 3',
+  title: "Vue Form Latte",
+  description: "Form generator library for Vue 3",
   themeConfig: {
-    nav: [
-      { text: "Guide", link: "/guide/getting-started" },
-      { text: "Config", link: "/config/" },
-      { text: "API", link: "/api/" },
-      { text: "Changelog", link: "/changelog/" },
-      // Github icon
-      {
-        text: 'GitHub',
-        link: 'https://github.com/Lithos-hub/vue-form-latte',
-      },
-    ],
-    sidebar: [
-      {
-        text: 'Introduction',
-        items: [
-          { text: 'What is Vue Form Latte', link: '/guide/what-is-vue-form-latte' },
-          { text: 'Getting Started', link: '/guide/getting-started' },
-        ]
-      },
-      {
-        text: 'Components',
-        items: [
-          { text: 'Input', link: '/guide/input' },
-          { text: 'Textarea', link: '/guide/textarea' },
-          { text: 'Select', link: '/guide/select' },
-          { text: 'Multiselect', link: '/guide/multiselect' },
-          { text: 'Radio', link: '/guide/radio' },
-          { text: 'Checkbox', link: '/guide/checkbox' },
-          { text: 'Slider', link: '/guide/slider' },
-        ]
-      }
-    ],
+    nav: nav(),
+    sidebar: sidebar(),
     search: {
-      provider: 'local'
+      provider: "local",
     },
     footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024-present Carlos Segura García'
-    }
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2024-present Carlos Segura García",
+    },
   },
 });
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+    { text: "Guide", link: "/guide/getting-started" },
+    { text: "Config", link: "/config/" },
+    { text: "API", link: "/api/" },
+    { text: "Changelog", link: "/changelog/" },
+    { text: "GitHub", link: "https://github.com/Lithos-hub/vue-form-latte" },
+  ];
+}
+
+function sidebar(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: "Introduction",
+      items: [
+        {
+          text: "What is Vue Form Latte",
+          link: "/guide/what-is-vue-form-latte",
+        },
+        { text: "Getting Started", link: "/guide/getting-started" },
+      ],
+    },
+    {
+      text: "Components",
+      items: [
+        { text: "Input", link: "/guide/input" },
+        { text: "Textarea", link: "/guide/textarea" },
+        { text: "Select", link: "/guide/select" },
+        { text: "Multiselect", link: "/guide/multiselect" },
+        { text: "Radio", link: "/guide/radio" },
+        { text: "Checkbox", link: "/guide/checkbox" },
+        { text: "Slider", link: "/guide/slider" },
+      ],
+    },
+  ];
+}
--- docs/guide/input.md
+++ docs/guide/input.md
@@ -7,6 +7,33 @@ The `input` component is a simple text input. It's a wrapper around the native `
 - `customStyles` (string, optional): Put here your CSS classes to customize the input. You can use Tailwind CSS classes or your own classes.
 - `label` (string, optional): The label of the input. If not provided, the input will not have a label.
 
+## Usage
+
+In order to use the `input` component, create an array of objects with the following structure:
+
+- **componentType** (string, required): The type of the component. In this case, it should be `input`.
+- **props** (object, required): The props of the component. This object should contain the `name` prop, so the input can be identified in the form's state. Also, you must provide the `initialValue` prop, so the input can have an initial value.
+
+```javascript
+const components = [
+  {
+    componentType: "input",
+    props: {
+      name: "name",
+      initialValue: "",
+    },
+  },
+];
+```
+
+Then, pass the `components` array to the `VueFormLatte` component:
+
+```vue
+<template>
+  <VueFormLatte :components="components" />
+</template>
+```
+
 ## Examples
 
 ### Basic input
